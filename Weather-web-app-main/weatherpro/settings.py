fe14cdf3bc2650e055c6df3143cc0330d87e7bfd
hitenpampaniya <hitendpampaniya33@gmail.com>
2026-06-03 17:34:47 +0530

    Configure static files and add WhiteNoise middleware
    Added WhiteNoise middleware for static file handling and updated STATIC_URL and STATIC_ROOT settings.

--- Weather-web-app-main/weatherpro/settings.py
+++ Weather-web-app-main/weatherpro/settings.py
@@ -45,14 +45,15 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+      'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
 ]
 
 ROOT_URLCONF = 'weatherpro.urls'
@@ -120,4 +121,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
